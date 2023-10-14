f9fe1a2b9a7e696e2c7c1c384770e9b3cc7db8cf
Yash Prakash <yashprakash13@gmail.com>
2023-10-14 12:04:40 -0400

    added ga4

--- src/ThanksPage.jsx
+++ src/ThanksPage.jsx
@@ -1,6 +1,12 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 const ThanksPage = () => {
+  useEffect(() => {
+    window.gtag("event", "form_submitted", {
+      event_category: "access",
+      event_label: "form_submitted_successfully",
+    })
+  }, [])
   return (
     <div className="flex h-screen items-center font-serif text-xl md:text-2xl ml-5 md:ml-16">
       We have your submission. Thank you for you choosing to work with us.{" "}
